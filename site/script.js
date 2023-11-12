6acca3b76e52e0e34236fa571a4b9842a24bfaf4
tiffknee <tiffkneee@gmail.com>
2023-11-12 23:19:02 +0100

    added fade in effect

---
+++ site/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+   
+        AOS.init({
+            offset: 100,
+            delay: 200,
+            duration: 1500,
+            once: true,
+            throttleDelay: 50,
+            disable: window.innerWidth < 768
+        });
+   
+
+});
